94d35ae2681a6124df1e5a888e85f7f4de52c875
WeriksonAlves <werikson.alves@ufv.br>
2025-06-15 21:49:19 -0300

    Add transform files for lab_scene_r step 3
    This commit introduces a series of new transform files (transforms_frame0000.npy to transforms_frame0059.npy) for the lab_scene_r step 3. These files are essential for the processing and analysis of the scene transformations.

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def stage3(scene: str, voxel_size=0.02) -> None:
         min_depth=0.01,
         max_depth=5.0
     )
-    batch.run(scale)
+    batch.run(scale+0.1)
 
     processor = DepthFusionProcessor(
         depth_real_dir=depth_sensor_dir,
@@ -93,23 +93,6 @@ def stage3(scene: str, voxel_size=0.02) -> None:
         output_dir=output_dir / "both",
     )
     processor.run(0)
-
-
-def run_capture_d12(
-    output_path,
-    max_frames=60,
-    fps=15
-) -> None:
-    """
-    Captures frames using RealSense and saves RGB, depth, and intrinsics.
-    """
-    recorder = RealSenseRecorder(
-        output_path=output_path,
-        max_frames=max_frames,
-        fps=fps
-    )
-    recorder.start()
-    recorder.capture()
 
 
 def run_compare_d5(
@@ -124,41 +107,6 @@ def run_compare_d5(
 
     comparer = PointCloudComparer(offset_apply=offset_apply)
     comparer.run([path_mono, path_d435], mode=0)
-
-
-def run_batch_alignment_d6(scene: str,
-                           voxel_size: float = 0.02) -> None:
-    """
-    Runs ICP alignment in batch for multiple frames.
-    """
-    batch = FrameICPAlignerBatch(
-        rgb_dir=Path(f"datasets/{scene}/rgb"),
-        depth_sensor_dir=Path(f"datasets/{scene}/depth_npy"),
-        depth_estimation_dir=Path(f"results_new/{scene}/d4/depth_npy"),
-        intrinsics_path=Path(f"datasets/{scene}/intrinsics.json"),
-        output_dir=Path(f"results_new/{scene}/d6"),
-        voxel_size=voxel_size,
-    )
-    scale, _ = batch.estimate_scale_from_depth_maps(
-        max_samples=50000,
-        min_depth=0.01,
-        max_depth=5.0
-    )
-    batch.run(scale)
-
-
-def run_fusion_d8(scene: str) -> None:
-    """
-    Performs depth map fusion between real and monocular.
-    """
-
-    results = Path(f"results_new/{scene}")
-    processor = DepthFusionProcessor(
-        depth_real_dir=Path(f"datasets/{scene}/depth_npy"),
-        depth_estimated_dir=results / "d4/depth_npy",
-        output_dir=results / "d8/both",
-    )
-    processor.run(0)
 
 
 def run_fused_reconstruction_d9(
@@ -189,7 +137,7 @@ def run_pipeline_sequence() -> None:
     """
     Executes a full pipeline step-by-step.
     """
-    scene = "lab_scene_f"
+    scene = "lab_scene_r"
     voxel_size = 0.02
 
     print(f"Running pipeline for scene: {scene}")
@@ -203,42 +151,11 @@ def run_pipeline_sequence() -> None:
     # # Stage 3: Align frames using ICP and fuse depth maps
     stage3(scene, voxel_size=voxel_size)
 
-
-
-
-
-
-
-
-
-
-
-
     # modules/reconstruction/rgbd_loader.py # Read .ply
-
-    # run_capture_d12(
-    #     output_path=Path(f"datasets/{scene}"),
-    #     max_frames=60,
-    #     fps=15
-    # )
-    # run_reconstruction_d3(scene, voxel_size)
-
-    # run_monodepth_d4(scene)
-    # run_reconstruction_d5(
-    #     scene,
-    #     voxel_size,
-    #     scale_correction=1  # Adjust as needed
-    # )
-
-    # run_batch_alignment_d6(scene, voxel_size=voxel_size)
-
-    # run_fusion_d8(scene)
-
     # run_fused_reconstruction_d9(
     #     scene,
     #     voxel_size
     # )
-
     # run_compare_d5(
     #     scene,
     #     offset_apply=False
